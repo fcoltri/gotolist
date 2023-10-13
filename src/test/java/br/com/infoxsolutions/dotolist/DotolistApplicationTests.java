becce1ee7d70fdb9e33fdc304fedb37618e594bb
Fernando Coltri <fcoltri@gmail.com>
2023-10-12 21:50:33 -0300

    inicio projeto

---
+++ src/test/java/br/com/infoxsolutions/dotolist/DotolistApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.infoxsolutions.dotolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DotolistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
